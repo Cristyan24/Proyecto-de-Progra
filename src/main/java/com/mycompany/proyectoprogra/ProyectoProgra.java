cb6ee6c6cbdeaa8d29696ac19ebd7f30d070d629
cristyan24 <cristyan24lopez@gmail.com>
2024-10-30 23:23:39 -0600

    Interfaz grafica prueba

---
+++ src/main/java/com/mycompany/proyectoprogra/ProyectoProgra.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprogra;
+
+/**
+ *
+ * @author user
+ */
+public class ProyectoProgra {
+
+    public static void main(String[] args) {
+        VentanaPrincipal nuevo = new VentanaPrincipal();
+        
+        nuevo.setVisible(true);
+    }
+}
